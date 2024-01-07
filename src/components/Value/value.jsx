85e864988e5a136a8f250b93f24a33405c0b38bf
Minh Nguyen <MNguyen@onc.ai>
2024-01-07 09:35:42 -0800

    commit

--- src/components/Value/value.jsx
+++ src/components/Value/value.jsx
@@ -6,7 +6,7 @@ const Value = () => {
   return (
     <motion.div
       initial={{ opacity: 0, x: -400 }}
-      whileInView={{ opacity: 1, when: "center", x: 0}}
+      whileInView={{ opacity: 1, x: 0}}
       transition={{ duration: 1.5 }}
     >
       <div className={style.aboutUsDescription}>
