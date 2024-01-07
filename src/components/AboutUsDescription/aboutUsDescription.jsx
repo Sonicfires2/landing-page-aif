4c1391b20484c9cd95c6020a9d0d7d1004492284
Minh Nguyen <MNguyen@onc.ai>
2024-01-07 09:50:50 -0800

    commit

--- src/components/AboutUsDescription/aboutUsDescription.jsx
+++ src/components/AboutUsDescription/aboutUsDescription.jsx
@@ -1,14 +1,8 @@
 import Tag from '../UIComponents/Tag/tag';
 import style from './aboutUsDescription.module.css'
-import { motion } from "framer-motion";
 
 const AboutUsDescription = () => {
   return (
-    // <motion.div
-    // initial={{ opacity: 0, x: -100 }}
-    // whileInView={{ opacity: 1, when: "center", x: 0}}
-    // transition={{ duration: 1.5 }}
-    // >
       <div className={style.aboutUsDescription}>
         <Tag content={"about-us"}></Tag>
         <h1 className={style.headings}>A Living Space.<br></br>Reimagined.</h1>
@@ -38,7 +32,6 @@ const AboutUsDescription = () => {
           </section>
         </p>
       </div>
-    {/* </motion.div> */}
   )
 }
 
