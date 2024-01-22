fdc709be36e090101a733f807c5e79a3a9c9c18b
Minh Nguyen <MNguyen@onc.ai>
2024-01-21 20:42:56 -0800

    your message here

--- src/components/Alum/alum.jsx
+++ src/components/Alum/alum.jsx
@@ -1,11 +1,16 @@
 import Tag from '../UIComponents/Tag/tag';
 import ProfileCard from './Profile/profileCard';
 import style from './alum.module.css';
-
+import {motion} from 'framer-motion'
 
 const Alum = () => {
   return (
-    <div className={style.alum}>
+    <motion.div
+      initial={{ opacity: 0, x: -100 }}
+      whileInView={{ opacity: 1, x: 0 }}
+      transition={{ duration: 1.5 }} 
+      className={style.alum}
+    >
       <Tag content={"Alumni"}></Tag>
       <h1 className={style.headings}>
         ALUMNI
@@ -16,7 +21,7 @@ const Alum = () => {
         <ProfileCard name={"Alexis"} major={"Brain + medical"} email={"No contact"}></ProfileCard>     
         <ProfileCard name={"Rina"} major={"No clue"} email={"No contact"}></ProfileCard>     
       </div>
-    </div>
+    </motion.div>
   )
 }
 
