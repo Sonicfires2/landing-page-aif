dd89cbfcbb2b30f83c3f0752bebb2cec952e62c4
Minh Nguyen <MNguyen@onc.ai>
2024-01-21 20:56:43 -0800

    your message here

--- src/components/UIComponents/Button/button.jsx
+++ src/components/UIComponents/Button/button.jsx
@@ -1,9 +1,21 @@
 import style from './button.module.css';
+import React, { useRef } from 'react';
 
 const HoverButton = () => {
+  function scrollDownBy200px() {
+    window.scrollBy({
+      top: 600, // 200 pixels down
+      left: 0,
+      behavior: 'smooth'
+    });
+  }
+  
+
   return (
-    <div className={style.container}>
-      <button className={style.learnMore}>Learn more</button>
+    <div 
+      className={style.container}
+    >
+      <button className={style.learnMore} onClick={scrollDownBy200px}>Learn more</button>
     </div>
   )
 }
